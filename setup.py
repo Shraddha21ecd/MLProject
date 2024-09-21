3538207733d92437d94a4b0b49d4859b5a8129a8
Shraddha Kumar <shraddha21ecd@ncuindia.edu>
2024-09-21 10:49:24 +0530

    first setup commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)-> List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n',"") for req in requirements]
+
+    return requirements
+
+
+
+
+
+setup(
+    name= 'MLProject',
+    version='0.0.1',
+    author='Shraddha',
+    author_email='shraddha20.4@gmail.com',
+    packages=find_packages(),
+    install_reqires=get_requirements('requirements.txt')
+
+)
